60e4ef009ef34db2477268580de34a2fbd581e0c
VedikaSagdeo123 <75945402+VedikaSagdeo123@users.noreply.github.com>
2021-03-07 13:54:20 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,26 @@
+class Ball{
+    constructor(){
+        var options={
+               restitution: 0.2,
+                density:0.8, 
+                friction:0.8
+             }
+             this.body = Bodies.circle(200,200,30,options);
+             this.radius=30;
+             this.image=loadImage("polygon.png")
+          World.add(world, this.body);
+          
+        }
+        display(){
+            var position=this.body.position
+            var angle=this.body.angle
+            push();
+            translate(position.x,position.y)
+            rotate(angle)
+            fill("purple")
+            image(this.image,0,0,this.radius,this.radius)
+            pop();
+          
+        
+        }
+    }
